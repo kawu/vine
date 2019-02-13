22e4f1b8d8626ad3599fa6998daa25ca6b32d109
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2019-02-13 10:26:20 +0100

    Graph network with arc labels

--- app/Main.hs
+++ app/Main.hs
@@ -10,13 +10,14 @@ import qualified Net.Ex2 as Ex2
 import qualified Net.Ex3 as Ex3
 import qualified Net.Ex4 as Ex4
 import qualified Net.DAG as DAG
-import qualified Net.Graph as Graph
+import qualified Net.ArcGraph as Graph
 import qualified Net.POS as POS
+import qualified Net.MWE2 as MWE
 import qualified GradientDescent.Momentum as Mom
 import qualified Embedding.Dict as D
 
 main :: IO ()
 main = do
   path : depth : _ <- getArgs
-  net <- POS.train path (read depth) =<< POS.new 300 5
+  net <- MWE.train path (read depth) =<< Graph.new 300 2
   return ()
